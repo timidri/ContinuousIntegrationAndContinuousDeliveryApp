d75986d8b1f67fecbd12e52cb5b5dc847eedf87e
Abir Majumdar <beersy@gmail.com>
2017-08-09 14:09:18 -0700

    Removing Docker tagging and pushing

--- dockerDeployment.sh
+++ dockerDeployment.sh
@@ -14,8 +14,8 @@ echo "Building Docker image"
 docker build -t $dockerImageName deployment/.
 echo "Running Docker container"
 docker run --net host --add-host jenkins.pdx.puppet.vm:192.168.0.101 -p 8090:8090 -d $dockerImageName
-dockerImageId=`docker images | grep $dockerImageName | grep latest | awk -F " " '{print $3}'` 
-docker tag $dockerImageId $dockerImageName:$version
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-echo "Pushing image to DockerHub"
-docker push $dockerImageName:$version
+#dockerImageId=`docker images | grep $dockerImageName | grep latest | awk -F " " '{print $3}'` 
+#docker tag $dockerImageId $dockerImageName:$version
+#docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+#echo "Pushing image to DockerHub"
+#docker push $dockerImageName:$version
