92fc40020c205a6da163b80e3a66c4711895fc82
Abir Majumdar <beersy@gmail.com>
2017-08-10 11:01:05 -0700

    Update isserverup.sh

--- isserverup.sh
+++ isserverup.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 SERVER=$1
 PORT=$2
-state=`nmap -p $PORT $SERVER | grep "$PORT" | grep open`
-if [ -z "$state" ]; then
-  echo "Connection to $SERVER on port $PORT has failed"
-  exit 1
-else
-  echo "Connection to $SERVER on port $PORT was successful"
-fi
-
+timeout 2 bash -c "</dev/tcp/$SERVER/$PORT"; echo $?
