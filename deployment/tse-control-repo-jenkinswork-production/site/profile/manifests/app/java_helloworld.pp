9d80a4f40fe93e60752bc6a8c6c8d445db946c25
Abir Majumdar <beersy@gmail.com>
2017-10-27 10:57:39 -0700

    Update java_helloworld.pp

--- deployment/tse-control-repo-jenkinswork-production/site/profile/manifests/app/java_helloworld.pp
+++ deployment/tse-control-repo-jenkinswork-production/site/profile/manifests/app/java_helloworld.pp
@@ -5,13 +5,22 @@ class profile::app::java_helloworld (
 ) {
 
   # Install Java
+#  java::oracle { 'jdk8' :
+#    ensure        => 'present',
+#    url_hash      => '090f390dda5b47b9b721c7dfaa008135',
+#    version_major => '8u144',
+#    version_minor => 'b01',
+#    java_se       => 'jdk',
+#  }
+
   java::oracle { 'jdk8' :
     ensure        => 'present',
-    url_hash      => '090f390dda5b47b9b721c7dfaa008135',
-    version_major => '8u144',
-    version_minor => 'b01',
+    version_major => '8u151',
+    version_minor => 'b12',
     java_se       => 'jdk',
+    url    => 'https://s3.amazonaws.com/saleseng/files/oracle/jdk-8u151-linux-x64.rpm',
   }
+
 
 
   # Install RPM
